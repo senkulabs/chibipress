55475da2ddc71bd1a4e992b1d39ffaaeeb67fa4f
Kresna Satya <kresnasatya72@gmail.com>
2025-06-29 17:44:57 +0800

    fix: change link for repository and documentation

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -27,7 +27,7 @@
                     <flux:navbar.item
                         class="h-10 max-lg:hidden [&>div>svg]:size-5"
                         icon="folder-git-2"
-                        href="https://github.com/laravel/livewire-starter-kit"
+                        href="https://github.com/kresnasatya/palm-cms"
                         target="_blank"
                         :label="__('Repository')"
                     />
@@ -36,7 +36,7 @@
                     <flux:navbar.item
                         class="h-10 max-lg:hidden [&>div>svg]:size-5"
                         icon="book-open-text"
-                        href="https://laravel.com/docs/starter-kits#livewire"
+                        href="/docs/api"
                         target="_blank"
                         label="Documentation"
                     />
