3e52ac1d99b7ed5a9fb036d6440c37768c8dbe27
Kresna Satya <kresnasatya72@gmail.com>
2025-06-29 17:40:04 +0800

    feat: initial RESTful API for Pages

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CategoryController as V1CategoryController;
+use App\Http\Controllers\Api\V1\PageController as V1PageController;
 use App\Http\Controllers\Api\V1\PostController as V1PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,4 +16,7 @@ Route::prefix('v1')->group(function () {
 
     Route::get('/categories', [V1CategoryController::class, 'index']);
     Route::get('/categories/{id}', [V1CategoryController::class, 'show']);
+
+    Route::get('/pages', [V1PageController::class, 'index']);
+    Route::get('/pages/{id}', [V1PageController::class, 'show']);
 });
